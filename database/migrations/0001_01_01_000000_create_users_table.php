34104038e96c42ed8e4a4db03ee1fc5a244102fe
NicoGutierrez95 <nicolas.nicanor.gutierrez95@gmail.com>
2026-05-24 11:44:47 -0300

    08 introduccion migraciones laravel finalizado

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id(); //metodo id(): devuelve int, unsigned, auto_increment
+            $table->string('name'); //metodo string(): devuelve varchar 255, si se quiere especificar el tamaño para cuestiones de memoria $table->string('name', 100);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            //table->text(); usado para mas de 255 caracteres
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
