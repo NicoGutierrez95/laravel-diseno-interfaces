e8f883f39f58974cc8b7a0cbc1d3e9737067a313
NicoGutierrez95 <nicolas.nicanor.gutierrez95@gmail.com>
2026-05-24 09:43:35 -0300

    06-vistas-en-laravel-finalizado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function __invoke(){ #método invoke, solo cuando queremos que el controlador administre una única ruta
         /*return view('welcome');*/
-        return "Bienvenido a la página principal";
+        return view('home');
     }
 }
---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos create')
+
+@section('content')
+    <h1>En esta página puedes crear tus cursos.</h1>
+@endsection
+
